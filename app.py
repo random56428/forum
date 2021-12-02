a2ae011fee52bc87de99691789023e2fd9aef247
random56428 <55150488+random56428@users.noreply.github.com>
2021-12-02 00:19:07 -0800

    Add register and login methods

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from temphelpers import apology, login_required
+from temphelpers import login_required
 
 # Configure application
 app = Flask(__name__)
@@ -34,11 +34,91 @@ db = SQL("sqlite:///forum.db")
 @login_required
 def index():
 
-    return apology("TODO")
+    return render_template("login.html")
 
-@app.route("/login")
+@app.route("/login", methods = ["GET", "POST"])
 def login():
 
-    flash("TEST")
-    return render_template("login.html")
+    # Forget any user_id
+    session.clear()
+
+    if request.method == "POST":
+        
+        # Check if username is blank
+        username = request.form.get("login_username")
+        if not username:
+            flash("Username input is blank.")
+            return render_template("login.html")
+
+        # Check if password is blank
+        password = request.form.get("login_password")
+        if not password:
+            flash("Password input is blank.")
+            return render_template("login.html")
+        
+        # Check if there is a username associated to request
+        # Where rows will return list of length 1 if an account is found
+        rows = db.execute("SELECT * FROM users WHERE username=?", username)
+        if len(rows) != 1:
+            flash("The username or password is incorrect.")
+            return render_template("login.html")
+
+        # Check if password matches db
+        if not check_password_hash(rows[0]["hash"], password):
+            flash("The username or password is incorrect.")
+            return render_template("login.html")
+        
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        flash("Successfully logged in.")
+        return redirect("/")
+    
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+        # Check if username is blank and already exists
+        username = request.form.get("register_username")
+
+        if not username:
+            flash("Username input is blank.")
+            return render_template("login.html")
+        elif len(db.execute("SELECT * FROM users WHERE username=?", username)) != 0:
+            flash("Username already exists.")
+            return render_template("login.html")
+            
+        password = request.form.get("register_password")
+        confirmation = request.form.get("register_confirmation")
+
+        # Check if password/confirmation is blank and if !=
+        if not password:
+            flash("Password input is blank.")
+            return render_template("login.html")
+        elif not confirmation:
+            flash("Confirmation password is blank.")
+            return render_template("login.html")
+        elif password != confirmation:
+            flash("Passwords do not match.")
+            return render_template("login.html")
+
+        # Register for user and insert into db
+        db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, generate_password_hash(password))
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username=?", username)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect to home page
+        flash("Successfully registered.")
+        return redirect("/")
+    else:
+        return redirect("/")
+
 
--- static/javascript.js
+++ static/javascript.js
@@ -1,31 +1,31 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector("#textareabutton").addEventListener('click', function () {
-        let textarea = document.querySelector("#floatingTextarea");
-        let typed = textarea.value;
-        textarea.value = "";
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelector("#textareabutton").addEventListener('click', function () {
+//         let textarea = document.querySelector("#floatingTextarea");
+//         let typed = textarea.value;
+//         textarea.value = "";
 
-        var str = `<hr>
-        <div class="post p-3">
-            <div class="row p-2">
-                Name placeholder
-            </div>
-            <div class="row p-2">
-                ${typed}
-            </div>
-            <div class="row p-2">
-                <div class="col">
-                    <button class="btn btn-default p-0">
-                        <img src="./static/images/upvote_light.png">
-                    </button>
-                    <label class="p-2">0</label>
-                    <button class="btn btn-default p-0">
-                        <img src="./static/images/downvote_light.png">
-                    </button>
-                </div>
-            </div>
-        </div>`
+//         var str = `<hr>
+//         <div class="post p-3">
+//             <div class="row p-2">
+//                 Name placeholder
+//             </div>
+//             <div class="row p-2">
+//                 ${typed}
+//             </div>
+//             <div class="row p-2">
+//                 <div class="col">
+//                     <button class="btn btn-default p-0">
+//                         <img src="./static/images/upvote_light.png">
+//                     </button>
+//                     <label class="p-2">0</label>
+//                     <button class="btn btn-default p-0">
+//                         <img src="./static/images/downvote_light.png">
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>`
         
-        document.querySelector(".post-holder").insertAdjacentHTML('afterbegin', str);
-    })
-});
+//         document.querySelector(".post-holder").insertAdjacentHTML('afterbegin', str);
+//     })
+// });
