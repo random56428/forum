23753ad5b8c23a08d2c36b8977e329cfc132f304
random56428 <55150488+random56428@users.noreply.github.com>
2021-12-01 18:35:07 -0800

    Add javacript for login posts

---
+++ static/javascript.js
@@ -0,0 +1,32 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector("#textareabutton").addEventListener('click', function () {
+        let textarea = document.querySelector("#floatingTextarea");
+        let typed = textarea.value;
+        textarea.value = "";
+
+        var str = `<hr>
+        <!--https://getbootstrap.com/docs/5.1/forms/floating-labels/-->
+        <div class="post p-3">
+            <div class="row p-2">
+                Name placeholder
+            </div>
+            <div class="row p-2">
+                ${typed}
+            </div>
+            <div class="row p-2">
+                <div class="col">
+                    <button class="btn btn-default p-0">
+                        <img src="./static/images/upvote_light.png">
+                    </button>
+                    <label class="p-2">0</label>
+                    <button class="btn btn-default p-0">
+                        <img src="./static/images/downvote_light.png">
+                    </button>
+                </div>
+            </div>
+        </div>`
+        
+        document.querySelector(".post-holder").insertAdjacentHTML('afterbegin', str);
+    })
+});
